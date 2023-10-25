10d29529e43c1c8d715cf891060b9a93b45a473c
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-25 11:19:51 +0200

    Update arguments_were_given.py

--- arguments_were_given.py
+++ arguments_were_given.py
@@ -1,8 +1,16 @@
+# Task 2
+
+# Write a program that, when run from the command line, reports how many
+# arguments were provided. (Remember that the program name itself is not an
+# argument)
+
 import sys
 
-if __name__ == "__main":
+if __name__ == "__main__":
     num_arguments = len(sys.argv) - 1
     if num_arguments == 0:
         print("No arguments provided.")
     else:
         print(f"Number of arguments provided: {num_arguments}")
+
+# python arguments_were_given.py argument1 arguments2 argument3
