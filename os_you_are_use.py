5b9f53761243c7dc429fbff9b38015bf9d9523d9
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-25 11:27:33 +0200

    Update os_you_are_use.py

--- os_you_are_use.py
+++ os_you_are_use.py
@@ -1,3 +1,7 @@
+# Using command-line arguments involves the sys module. Review the docs for this
+# module and using the information in there write a short program that when run
+# from the command-line reports what operating system platform is being used.
+
 import sys
 
 
@@ -8,3 +12,14 @@ def os_you_use():
 if __name__ == "__main__":
     platform = os_you_use()
     print("Operating System Platform:", platform)
+
+# python os_you_are_use.py
+
+
+# or another solution
+# def os_you_use():
+#     return sys.platform
+# 
+#
+# platform = os_you_use()
+# print("Operating System Platform:", platform)
