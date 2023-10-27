e83237cd7ffad8e38e781d51034621990a4be1c3
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-28 00:35:22 +0200

    Update backup_file.py

--- backup_file.py
+++ backup_file.py
@@ -1,3 +1,5 @@
+# Task 5
+
 # Write a program that takes the name of a file as a command-line argument, and
 # creates a backup copy of that file. The backup should contain an exact copy of the
 # contents of the original and should, obviously, have a different name.
