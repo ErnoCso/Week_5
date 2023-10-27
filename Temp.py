471a17034e13e1a43b9f5eb57dbdfebdeebccbb3
ErnoCso <147259999+ErnoCso@users.noreply.github.com>
2023-10-28 00:13:23 +0200

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,23 +1,34 @@
+# Task5
+
+# Last week you wrote a program that processed a collection of temperature readings
+# entered by the user and displayed the maximum, minimum, and mean. Create a
+# version of that program that takes the values from the command-line instead. Be
+# sure to handle the case where no arguments are provided
+
 import sys
 
-def highestLowest(temperatures):
-    if not temperatures:
+def highestlowest(temps):
+    if not temps:
         print("No temperature values specified.")
     else:
-        print(f"Lowest Temperature: {min(temperatures)}°C")
-        print(f"Highest Temperature: {max(temperatures)}°C")
+        print(f"Lowest Temperature: {min(temps)}°C")
+        print(f"Highest Temperature: {max(temps)}°C")
 
-def average(temperatures):
-    if not temperatures:
+
+def average(temps):
+    if not temps:
         print("No temperature value specified.")
     else:
-        avg = sum(temperatures) / len(temperatures)
+        avg = sum(temps) / len(temps)
         print(f"Average temperature: {avg}°C")
 
-if __name__ == "__main":
-    arguments = sys.argv[1:]  # We omit the first argument, which is the name of the program
 
-    temperatures = [int(temp) for temp in arguments]  # Convert arguments to integers
+arguments = sys.argv[1:]  # We omit the first argument, which is the name of the program
 
-    highestLowest(temperatures)
-    average(temperatures)
+temps = [float(temp) for temp in arguments]  # Convert arguments to floats
+
+highestlowest(temps)
+average(temps)
+
+
+# python Test.py 12.5 21.3 23.0 25 26.4 30 34.4 29.1 20.7
